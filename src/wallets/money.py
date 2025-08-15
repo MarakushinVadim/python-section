fee09801da24f75a7fbd4312a2dfc454be5c8a4e
Vadim <vadim.marakushin@gmail.com>
2025-08-15 16:26:43 +0300

    Money

--- src/wallets/money.py
+++ src/wallets/money.py
@@ -1,12 +1,9 @@
-from typing_extensions import TypeAlias
-
-from src.wallets.currency import Currency, Rub
+from src.wallets.currency import Currency, rub, usd
 from src.wallets.exceptions import NotComparisonException
 
-CurrencyType: TypeAlias = list[Currency]
 
 class Money:
-    def __init__(self, value: float, currency: Currency):
+    def __init__(self, value: int, currency: Currency):
         self.value = value
         self.currency = currency
 
@@ -27,8 +24,45 @@ class Money:
 
 
 class Wallet:
-
-
     def __init__(self, money: Money):
-        self.currencies = [money]
+        self.currencies = self.set_start_currencies(money)
+
+    def __getitem__(self, item: Currency):
+        return self.check_currency(item)
+
+    def __setitem__(self, key: Currency, value: int):
+        obj = self.check_currency(key)
+        if obj:
+            obj.value += value
+        else:
+            self.currencies.append(Money(value=value, currency=key))
+
+    def __delitem__(self, key: Currency):
+        obj = self.check_currency(key)
+        if obj:
+            self.currencies.remove(obj)
+
+    def __len__(self):
+        return len(self.currencies)
+
+    def __contains__(self, item: Currency):
+        return item in self.currencies
+
+    def add(self, money: Money) -> None:
+        obj = self.check_currency(money.currency)
+        if obj:
+            obj.value += money.value
+
+
+    def check_currency(self, other: Currency):
+        for money in self.currencies:
+            if money.currency == other:
+                return money
+        return None
+
+    @staticmethod
+    def set_start_currencies(money: Money):
+        if money.currency == rub:
+            return [money, Money(value=0, currency=usd)]
+        return [Money(value=0, currency=rub), money]
 
