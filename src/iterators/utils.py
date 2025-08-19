a57355743f0e60dbb98bbb8c4c1c9549cf902686
Vadim <vadim.marakushin@gmail.com>
2025-08-19 14:55:23 +0300

    Utils done

--- src/iterators/utils.py
+++ src/iterators/utils.py
@@ -29,8 +29,50 @@ def request(query: Query) -> Page:
 
 
 class RetrieveRemoteData:
-    pass
+
+    def __init__(self, per_page: int = 3):
+        self.per_page = per_page
+
+    def __iter__(self) -> Iterable[SomeRemoteData]:
+        return self.take_generator()
+
+    def take_generator(self) -> Iterable[SomeRemoteData]:
+
+        counter = 1
+        x = True
+        while x:
+            page_obj = request(Query(per_page=self.per_page, page=counter))
+            for obj in page_obj.results:
+                yield obj
+
+            if page_obj.next is None:
+                x = False
+            counter += 1
 
 
 class Fibo:
-    pass
+
+    def __init__(self, n: int, start=0):
+        self.index = 0
+        self.end = n
+        self.start = start
+        self.last = [0, 1]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> int:
+        if self.index == self.end:
+            raise StopIteration
+        self.index += 1
+
+        if self.index == 1:
+            return 0
+
+        elif self.index == 2:
+            return 1
+
+        result = sum(self.last)
+        self.last = [self.last[1], result]
+
+        return result
