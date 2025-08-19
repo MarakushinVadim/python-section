9f184d1875ed794e46784ccf31e3941179e2fb63
Vadim <vadim.marakushin@gmail.com>
2025-08-19 14:55:15 +0300

    Models done

--- src/descriptors/models.py
+++ src/descriptors/models.py
@@ -9,4 +9,26 @@ class Model:
 
 
 class Field:
-    pass
+    def __init__(self, path: str):
+        self.path = path.split(".")
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        data = instance.payload
+        try:
+            for i in range(len(self.path)):
+                data = data.get(self.path[i])
+        except AttributeError:
+            return None
+        return data
+
+    def __set__(self, instance, value):
+        data = instance.payload
+        for key in self.path:
+
+            if key == self.path[-1]:
+                data[key] = value
+            else:
+                data = data[key]
