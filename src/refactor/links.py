a8cf1914c9f8894b8dead5c201d5508537373777
Vadim <vadim.marakushin@gmail.com>
2025-08-20 14:19:37 +0300

    Links done

--- src/refactor/links.py
+++ src/refactor/links.py
@@ -13,24 +13,21 @@ def parse_page_links(html: str, start_date: date, end_date: date, url: str):
     soup = BeautifulSoup(html, "html.parser")
     links = soup.find_all("a", class_="accordeon-inner__item-title link xls")
 
+    def validity_check(link_href: str) -> bool:
+        return "/upload/reports/oil_xls/oil_xls_" in link_href and link_href.endswith(".xls")
+
     for link in links:
         href = link.get("href")
-        if not href:
-            continue
-
-        href = href.split("?")[0]
-        if "/upload/reports/oil_xls/oil_xls_" not in href or not href.endswith(".xls"):
-            continue
-
-        try:
-            date = href.split("oil_xls_")[1][:8]
-            file = datetime.datetime.strptime(date, "%Y%m%d").date()
-            if start_date <= file <= end_date:
-                u = href if href.startswith("http") else f"https://spimex.com{href}"
-                results.append((u, file))
-            else:
-                print(f"Ссылка {href} вне диапазона дат")
-        except Exception as e:
-            print(f"Не удалось извлечь дату из ссылки {href}: {e}")
-
+        if href:
+            href = href.split("?")[0]
+            if validity_check(href):
+                try:
+                    file_date = datetime.datetime.strptime(href.split("oil_xls_")[1][:8], "%Y%m%d").date()
+                    if start_date <= file_date <= end_date:
+                        u = href if href.startswith("http") else f"https://spimex.com{href}"
+                        results.append((u, file_date))
+                    else:
+                        print(f"Ссылка {href} вне диапазона дат")
+                except Exception as e:
+                    print(f"Не удалось извлечь дату из ссылки {href}: {e}")
     return results
